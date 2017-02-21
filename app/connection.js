436c36e0c29b9ea05fff9f1282ca7c9f34442d48
Mahdan Ahmad Fauzi Al-Hasan <mahdanahmad@users.noreply.github.com>
2017-02-21 11:22:10 +0700

    Add additional function to connection

--- app/connection.js
+++ app/connection.js
@@ -1,25 +1,29 @@
-let MongoClient = require('mongodb').MongoClient
+const MongoDB		= require('mongodb');
+const MongoClient	= MongoDB.MongoClient;
+const ObjectID		= MongoDB.ObjectID;
 
-let state = { db : null }
+const state 	= { db: null };
 
-exports.connect = function(url, callback) {
-    if (state.db) return callback();
+exports.connect = (url, callback) => {
+	if (state.db) return callback();
 
-    MongoClient.connect(url, function(err, db) {
-        if (err) return callback(err);
-        state.db    = db;
-        callback();
-    })
-}
+	MongoClient.connect(url, (err, db) => {
+		if (err) return callback(err);
+		state.db    = db;
+		callback();
+	});
+};
 
-exports.get = function() { return state.db; }
+exports.close = (callback) => {
+	if (state.db) {
+		state.db.close((err, result) => {
+			state.db = null;
+			state.mode = null;
+			callback(err);
+		});
+	}
+};
 
-exports.close = function(callback) {
-    if (state.db) {
-        state.db.close(function(err, result) {
-            state.db = null;
-            state.mode = null;
-            callback(err);
-        })
-    }
-}
+exports.get 			= () => (state.db);
+exports.toObjectID      = (stringID) => (ObjectID.isValid(stringID) ? new ObjectID(stringID) : null);
+exports.getCollection   = (table) => (state.db.collection(table));
